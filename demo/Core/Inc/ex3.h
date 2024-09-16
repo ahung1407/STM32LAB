a5630ee2afa712225aa9b61597bd97d88d8ad424
Minh Hung <hungminh09065@gmail.com>
2024-09-16 10:28:49 +0700

    newlife

---
+++ demo/Core/Inc/ex3.h
@@ -0,0 +1,16 @@
+/*
+ * ex3.h
+ *
+ *  Created on: Sep 11, 2024
+ *      Author: admim
+ */
+
+#ifndef INC_EX3_H_
+#define INC_EX3_H_
+#include "main.h"
+
+void init_excercise3();
+void run_excercise3();
+
+
+#endif /* INC_EX3_H_ */
---
+++ demo/Core/Inc/ex4.h
@@ -0,0 +1,17 @@
+/*
+ * ex4.h
+ *
+ *  Created on: Sep 12, 2024
+ *      Author: admim
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+
+#include "main.h"
+
+void init_excercise4();
+void run_excercise4(int counter);
+
+
+#endif /* INC_EX4_H_ */
